fe92417a7cb75d667dc18450514a095e2f24feef
Filipe Pereira <filipe.pereira.dev@gmail.com>
2023-11-25 17:39:25 +0100

    Change report + fix sessions demo

--- demos/creationSessions.c
+++ demos/creationSessions.c
@@ -2,22 +2,23 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <errno.h>
-
+#include <stdlib.h>  
 
 int main(void) {
 
-    if (fork() != 0) /* Exit if parent, or on error */
+    if (fork() != 0) // Exit du parent
         exit(EXIT_SUCCESS);
 
     if (setsid() == -1)
-        errExit("setsid");
+        perror("setsid");
+        exit(-1);
 
     printf("PID=%ld, PGID=%ld, SID=%ld\n", (long) getpid(),
         (long) getpgrp(), (long) getsid(0));
 
-    if (open("/dev/tty", O_RDWR) == -1)
-        errExit("open /dev/tty");
+    if (open("/dev/tty", O_RDWR) == -1) // Ouvrir terminal de controle associé
+        perror("open /dev/tty");
+        exit(-1);
 
-    exit(EXIT_SUCCESS);
-
+    exit(0);
 }
